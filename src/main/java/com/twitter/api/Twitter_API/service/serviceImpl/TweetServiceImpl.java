f8084bbf7ffc923d46da8a32bfd6a5b5cd1873b1
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-04-08 14:00:18 +0300

    tweet service created

---
+++ src/main/java/com/twitter/api/Twitter_API/service/serviceImpl/TweetServiceImpl.java
@@ -0,0 +1,8 @@
+package com.twitter.api.Twitter_API.service.serviceImpl;
+
+import com.twitter.api.Twitter_API.service.TweetService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TweetServiceImpl implements TweetService {
+}
