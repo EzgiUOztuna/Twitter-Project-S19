2f7fbb90828b35c6fa8387ec6ba41ccb20a66db1
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-04-08 14:04:33 +0300

    user controller created

---
+++ src/main/java/com/twitter/api/Twitter_API/controller/controllerImpl/UserControllerImpl.java
@@ -0,0 +1,7 @@
+package com.twitter.api.Twitter_API.controller.controllerImpl;
+import com.twitter.api.Twitter_API.controller.UserController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserControllerImpl implements UserController {
+}
